3248f88c095ac3e3cc19ed96ce0ac7272662d248
Magnus Andersson <mange@mange.name>
2013-07-10 00:14:24 -0400

    Update vimified install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
+VIMIFIED_PATH=~/.vimified
+
 # Grab the project:
-cd
-git clone git://github.com/snichme/vimified.git ~/.vimified
-ln -sfn ~/.vimified ~/.vim
-ln -sfn ~/.vimified/vimrc ~/.vimrc
-cd ~/.vimified
+git clone git://github.com/snichme/vimified.git $VIMIFIED_PATH
+ln -sfn $VIMIFIED_PATH ~/.vim
+ln -sfn $VIMIFIED_PATH/vimrc ~/.vimrc
 
 #Create required directories
-mkdir bundle
-mkdir -p tmp/backup tmp/swap tmp/undo
+mkdir $VIMIFIED_PATH/bundle
+mkdir -p $VIMIFIED_PATH/tmp/backup $VIMIFIED_PATH/tmp/swap $VIMIFIED_PATH/tmp/undo
 
 #Set up Vundle
-git clone https://github.com/gmarik/vundle.git bundle/vundle
+git clone https://github.com/gmarik/vundle.git $VIMIFIED_PATH/bundle/vundle
 
 #Set up your local configuration via local.vimrc file and choose package you want to use:
 #echo "let g:vimified_packages = ['general', 'coding', 'clojure', 'color']" > local.vimrc
 
-ln -sfn local.vimrc ~/.vim/local.vimrc
-ln -sfn extra.vimrc ~/.vim/extra.vimrc
-
+ln -sfn $VIMIFIED_PATH/local.vimrc ~/.vim/local.vimrc
+ln -sfn $VIMIFIED_PATH/extra.vimrc ~/.vim/extra.vimrc
 
 # Install plugins:
-vim +BundleInstall +qall
+vim +BundleInstall +qall 2>/dev/null
