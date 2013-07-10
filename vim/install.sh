88bf0029e0018071e9a872b4a5ac9325b28dbb09
Magnus Andersson <mange@mange.name>
2013-07-10 14:54:25 -0400

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -14,11 +14,5 @@ mkdir -p $VIMIFIED_PATH/tmp/backup $VIMIFIED_PATH/tmp/swap $VIMIFIED_PATH/tmp/un
 #Set up Vundle
 git clone https://github.com/gmarik/vundle.git $VIMIFIED_PATH/bundle/vundle
 
-#Set up your local configuration via local.vimrc file and choose package you want to use:
-#echo "let g:vimified_packages = ['general', 'coding', 'clojure', 'color']" > local.vimrc
-
-ln -sfn $VIMIFIED_PATH/local.vimrc ~/.vim/local.vimrc
-ln -sfn $VIMIFIED_PATH/extra.vimrc ~/.vim/extra.vimrc
-
 # Install plugins:
-vim +BundleInstall +qall 2>/dev/null
+vim +BundleInstall +qall
