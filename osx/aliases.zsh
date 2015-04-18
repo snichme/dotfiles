b405e7c47063cfe9a71fa78583921bc65322f38a
Magnus Landerblom <magnus@landerblom.se>
2015-04-18 23:00:58 +0200

    Moved OSX specific aliases to osx folder

---
+++ osx/aliases.zsh
@@ -0,0 +1,6 @@
+# alias emacs-server='~/Applications/Emacs.app/Contents/MacOS/Emacs --daemon'
+# alias emacs='~/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -t'
+# alias emacs-gui='~/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -c'
+
+alias emacs='~/Applications/Emacs.app/Contents/MacOS/Emacs'
+alias emacsclient='~/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
